6fcfa4cf5033dd640755d210361bb6e432be0246
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-08-10 22:15:36 -0300

    last steps working on product list

--- personal-projects/virtual-floriculture-gardening-store/scripts/script.js
+++ personal-projects/virtual-floriculture-gardening-store/scripts/script.js
@@ -17,3 +17,38 @@ window.onscroll = function scrolling(){
         goTopButton.classList.remove('active')
     }
 }
+
+//STORE
+let searchBar = document.querySelector('#search-bar')
+let cleanButton = document.querySelector('.clean-button')
+
+//Select products categories
+function SetCategoriesState(currentButton){
+    if (currentButton.classList.contains('categorie-selected')){
+        currentButton.classList.remove('categorie-selected')
+    }else{
+        let allButtons = document.getElementsByClassName('categories-bar-buttons')
+        for (let i = 0; i < allButtons.length; i++){
+            if (allButtons[i].classList.contains('categorie-selected')){
+                allButtons[i].classList.remove('categorie-selected')
+            }
+        }
+        currentButton.classList.add('categorie-selected')
+    }
+    
+}
+
+//clean search bar button appears and disappears
+function cleanButtonState(){
+    if (searchBar.value != ''){
+        cleanButton.style.display = 'block'
+    }else{
+        cleanButton.style.display = 'none'
+    }
+}
+
+//clean search bar
+function cleanSearchBar(){
+    searchBar.value = ''
+    cleanButton.style.display = 'none'
+}
