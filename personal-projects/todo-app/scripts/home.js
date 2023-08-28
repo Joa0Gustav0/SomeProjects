32ecd74399141b513f3cca6ab83c73749bb4d58b
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-08-28 19:00:14 -0300

    sidebar done

--- personal-projects/todo-app/scripts/home.js
+++ personal-projects/todo-app/scripts/home.js
@@ -1,24 +1,24 @@
 //change sidebar state
+const sideBar = document.querySelector('.hero-section__sidebar')
 const sideBarButton = document.querySelector('.hero-section__menu-button')
 const screenSaver = document.querySelector('.hero-section__screen-saver')
 
 function setSideBarState(){
-    function setSideBarButtonState(){
-        if (sideBarButton.classList.contains('opened') == false){
-            screenSaver.style.display = 'block'
-            sideBarButton.classList.add('semi-opened')
-            function endProcess(){
-                sideBarButton.classList.add('opened')
-            }
-            setTimeout(endProcess, 250)
-        }else{
-            screenSaver.style.display = 'none'
-            sideBarButton.classList.remove('opened')
-            function endProcess(){
-                sideBarButton.classList.remove('semi-opened')
-            }
-            setTimeout(endProcess, 250)
+    if (sideBarButton.classList.contains('opened') == false){
+        screenSaver.style.display = 'block'
+        sideBar.classList.add('opened')
+        sideBarButton.classList.add('semi-opened')
+        function endProcess(){
+            sideBarButton.classList.add('opened')
         }
+        setTimeout(endProcess, 250)
+    }else{
+        sideBar.classList.remove('opened')
+        sideBarButton.classList.remove('opened')
+        function endProcess(){
+            sideBarButton.classList.remove('semi-opened')
+        }
+        setTimeout(endProcess, 250)
+        screenSaver.style.display = 'none'
     }
-    setSideBarButtonState()
 }
