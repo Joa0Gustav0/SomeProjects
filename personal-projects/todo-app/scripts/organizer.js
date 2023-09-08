a974639ead6fcbfb79049081b61d9751d2fc5c97
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-09-07 22:40:24 -0300

    saving current state

--- personal-projects/todo-app/scripts/organizer.js
+++ personal-projects/todo-app/scripts/organizer.js
@@ -1,6 +1,9 @@
 const addCategoriesButton = document.querySelector('.to-do-section__add-categorie-button')
+const removeCategorieButton = document.querySelectorAll('.to-do-section__categorie-containers__delete-categorie-button')
 
+//Create new containers
 addCategoriesButton.addEventListener('click', () => {
+    //create container
     const categorieContainerModel = document.querySelector('.to-do-section__other-categorie-containers-model-example')
     let newCategorieContainer = document.createElement('div')
     newCategorieContainer.innerHTML = categorieContainerModel.innerHTML
@@ -13,4 +16,86 @@ addCategoriesButton.addEventListener('click', () => {
 
     const newCategorieContainerName = newCategorieContainer.childNodes[1]
     newCategorieContainerName.innerHTML = `<ion-icon name="book"></ion-icon> Other ${otherCategoriesContainers.length}`
+
+    reorganizeCreatedContainers()
+    
+    //create container tab
+    const categorieTabModel = document.querySelector('.to-do-section__alt-categories-tabs-container__to-do-categories-tab')
+
+    let newCategorieTab = document.createElement('div')
+    newCategorieTab.innerHTML = categorieTabModel.innerHTML
+    newCategorieTab.className = `to-do-section__alt-categories-tabs-container__to-do-categories-tab to-do-section__alt-categories-tabs-container__other-categories-tab to-do-section__alt-categories-tabs-container__other${otherCategoriesContainers.length}-categories-tab`
+    newCategorieTab.id = `other${otherCategoriesContainers.length}`
+    newCategorieTab.childNodes[3].innerText = `Other ${otherCategoriesContainers.length}`
+
+    const categoriesTabsContainer = document.querySelector('.to-do-section__alt-categories-tabs-container')
+    categoriesTabsContainer.appendChild(newCategorieTab)
 })
+
+//Remove containers
+const removeCurrentContainer = (currentButton) =>{
+    currentButton.parentNode.remove()
+    reorganizeCreatedContainers()
+}
+
+//rename other's containers
+const reorganizeCreatedContainers = () => {
+    const otherContainers = document.getElementsByClassName('to-do-section__other-categorie-containers')
+
+    for (var i = 0; i < otherContainers.length; i++){
+        otherContainers[i].className = `to-do-section__categorie-containers to-do-section__other-categorie-containers to-do-section__other${i + 1}-categorie-containers`
+        otherContainers[i].childNodes[1].innerHTML = `<ion-icon name="book"></ion-icon> Other ${i + 1}`
+    }
+}
+
+/*RESPONSIVINESS*/
+const allContainers = document.getElementsByClassName('to-do-section__categorie-containers')
+const allOtherContainers = document.getElementsByClassName('to-do-section__other-categorie-containers')
+let matchTheSize = false
+
+window.onload = () => {
+    if (window.matchMedia('(max-width: 759px)').matches){
+        if (matchTheSize == false){
+            for (var i = 0; i < allContainers.length; i++){
+                allContainers[i].style.display = 'none'
+            }
+            allContainers[0].style.display = 'flex'
+            matchTheSize = true
+        }
+    }
+    if (window.matchMedia('(min-width: 760px)').matches){
+        for (var i = 0; i < allContainers.length; i++){
+            allContainers[i].style.display = 'flex'
+        }
+        matchTheSize = false
+    }
+}
+
+window.addEventListener('resize', () => {
+    if (window.matchMedia('(max-width: 759px)').matches){
+        if (matchTheSize == false){
+            for (var i = 0; i < allContainers.length; i++){
+                allContainers[i].style.display = 'none'
+            }
+            allContainers[0].style.display = 'flex'
+            matchTheSize = true
+        }
+    }
+    if (window.matchMedia('(min-width: 760px)').matches){
+        for (var i = 0; i < allContainers.length; i++){
+            allContainers[i].style.display = 'flex'
+        }
+        matchTheSize = false
+    }
+})
+
+//navigate through containers using the tabs
+const setCurrentContainerVisible = (currentTab) => {
+    const currentCategorie = document.querySelector(`.to-do-section__${currentTab.id}-categorie-container`)
+
+    for (var i = 0; i < allContainers.length; i++){
+        allContainers[i].style.display = 'none'
+    }
+    currentCategorie.style.display = 'flex'
+}
+    
