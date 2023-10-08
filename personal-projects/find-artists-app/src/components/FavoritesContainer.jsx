1b7c8262fd349e83cf1bd07904a4b55652167e2f
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-10-08 19:26:05 -0300

    🏗 Creating Favorites List interactivity | Aurora

--- personal-projects/find-artists-app/src/components/FavoritesContainer.jsx
+++ personal-projects/find-artists-app/src/components/FavoritesContainer.jsx
@@ -1,14 +1,18 @@
 import whiteLogo from '../media/aurora-logo-white.png'
 import styles from './styles/FavoritesContainer.module.css'
 
-export default function FavoritesContainer(){
+export default function FavoritesContainer({favoritesArray}){
+    console.log(favoritesArray.lenght)
     return (
         <div className={styles.favcontainer}>
             <h1 className={styles.favTitle}><ion-icon name="heart"></ion-icon>Favorites:</h1>
             <div className={`favoritesList ${styles.favList}`}>
-                <img className={styles.nofavImage} src={whiteLogo} alt="aurora-logo"/>
-                <h1 className={styles.nofavText}>It seens you do not have 
-any favorites...</h1>
+                {favoritesArray.length === 0 && (
+                    <>
+                        <img className={styles.nofavImage} src={whiteLogo} alt="aurora-logo"/>
+                        <h1 className={styles.nofavText}>It seens you do not have any favorites...</h1>
+                    </>
+                )}
             </div>
         </div>
     )
--- personal-projects/find-artists-app/src/components/Finder.jsx
+++ personal-projects/find-artists-app/src/components/Finder.jsx
@@ -1,10 +1,13 @@
 import FavoritesContainer from './FavoritesContainer'
 import styles from './styles/Finder.module.css'
+import { useState } from 'react'
 
 export default function Finder(){
+    const [favorites, setFavorites] = useState([])
+
     return (
         <main>
-            <FavoritesContainer />
+            <FavoritesContainer favoritesArray={favorites}/>
         </main>
     )
 }
