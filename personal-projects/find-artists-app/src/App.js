dc9efb877edf0530e60447693cc727f7606d90dd
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-10-08 16:08:28 -0300

    🏗 Creating Finder Component | Aurora

--- personal-projects/find-artists-app/src/App.js
+++ personal-projects/find-artists-app/src/App.js
@@ -1,9 +1,11 @@
 import Home from "./components/Home";
+import Finder from "./components/Finder";
 
 function App() {
   return (
     <div className="Container">
       <Home />
+      <Finder />
     </div>
   );
 }
