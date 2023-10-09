f96c9beff9f4fcb62fd3e5954ec422970feb20ac
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-10-09 19:53:58 -0300

    🏗 SearchBar state script done | Aurora

--- personal-projects/find-artists-app/src/components/SearchBar.jsx
+++ personal-projects/find-artists-app/src/components/SearchBar.jsx
@@ -1,12 +1,22 @@
 import styles from './styles/SearchBar.module.css'
+import { useState } from 'react'
 
 export default function SearchBar(){
+
+    const [divClass, setDivClass] = useState(styles.SearchBarContainer)
+
     return (
-        <div className={styles.SearchBarContainer}>
+        <div className={divClass}>
             <label htmlFor="search-bar">
                 <ion-icon name="search"></ion-icon>
             </label>
-            <input type="text" id="search-bar" placeholder='Let Aurora guide you...'/>
+            <input type="text" id="search-bar" placeholder='Let Aurora guide you...' onChange={(e) => {
+                if (e.target.value !== ""){
+                    setDivClass(`${styles.SearchBarContainer} ${styles.onSearch}`)
+                }else{
+                    setDivClass(styles.SearchBarContainer)
+                }
+            }}/>
         </div>
     )
 }
