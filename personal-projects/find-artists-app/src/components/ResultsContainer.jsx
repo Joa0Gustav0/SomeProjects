ddf1e66a1879378ae4ed9bff9cfb7d196d28dd14
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-10-12 14:33:09 -0300

    🏗 Favorite marking when listed added | Aurora

--- personal-projects/find-artists-app/src/components/ResultsContainer.jsx
+++ personal-projects/find-artists-app/src/components/ResultsContainer.jsx
@@ -64,7 +64,7 @@ export default function ResultsContainer({favoriteEvent, favArr}){
                                 <h1 className={styles.artistName}>{currentArtist.name}</h1>
                             </abbr>
                             <h1 className={styles.artistCategorie}>Artist</h1>
-                            <ion-icon name={favArr.includes(currentArtist.id) ? "heart" : "heart-outline"} id={currentArtist.id} onClick={(e) => {
+                            <ion-icon name={favArr.findIndex((elem) => elem.id === currentArtist.id) > -1 ? "heart" : "heart-outline"} id={currentArtist.id} onClick={(e) => {
                                 favoriteEvent({id: currentArtist.id, name: currentArtist.name, img: currentArtist.images[0]?.url || noArtistPicture})
                             }}></ion-icon>
                         </div>
