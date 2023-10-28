c68a93236f9e3d1c93ac190f0b8a91deb90daaf2
Joao <guga67423@outlook.com>
2023-10-28 15:01:53 -0300

    testing api main call

--- personal-projects/quotation-api-app/src/App.js
+++ personal-projects/quotation-api-app/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={async () => {
+
+        await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&")
+        .then(data => data.json())
+        .then(data => console.log(data))
+
+      }}>Try it!</button>
     </div>
   );
 }
