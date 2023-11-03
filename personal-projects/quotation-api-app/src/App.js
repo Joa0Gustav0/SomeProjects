56b60a0bce05812e3ef550df788ea5715e36f41f
Joao <guga67423@outlook.com>
2023-11-03 04:37:22 -0300

    added a go top button

--- personal-projects/quotation-api-app/src/App.js
+++ personal-projects/quotation-api-app/src/App.js
@@ -3,10 +3,14 @@ import Hero from "./components/Hero";
 import Currencies from "./components/Currencies";
 import Services from "./components/Services";
 import styles from './App.module.css'
+import goTopButtonPic from './components/media/go-top-button.png'
+import { useState } from "react";
 
 function App() {
 
-  
+  const [goTopButton, setGoTopButton] = useState(styles.goTopButton)
+
+  window.onscroll = () => window.scrollY < 100 ? setGoTopButton(styles.goTopButton) : setGoTopButton(`${styles.goTopButton} ${styles.active}`)
 
   return (
     <>
@@ -16,6 +20,9 @@ function App() {
         <Currencies />
         <Services />
       </main>
+      <a className={goTopButton} href="#home">
+        <img src={goTopButtonPic} alt="go-top-button"/>
+      </a>
 
       {/* <div className={styles.container}>
         <DataContainer initIndexPage={initPage}/>
