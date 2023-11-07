d7a9d97d5a433bd1ff5aca9a964d4ce8a9c0f43e
Joao <guga67423@outlook.com>
2023-11-07 17:24:51 -0300

    added style to products container | dashboard app

--- personal-projects/dashboard-app/src/components/DataList.jsx
+++ personal-projects/dashboard-app/src/components/DataList.jsx
@@ -3,15 +3,17 @@ import styles from './styles/DataList.module.css'
 export default function DataList( {productsArr} ) {
     return (
         <aside className={styles.dataListContainer}>
-            <h1>Products</h1>
+            <h1>Products <span>({productsArr.length} items)</span></h1>
             <div className={styles.dataList__List} style={productsArr?.length > 0 ? {justifyContent: "flex-start"} : {justifyContent: "center"}}>
 
                 {
                     productsArr?.length > 0 ?
                     productsArr.map((elem, i) => (
                         <div key={elem.name + i} className={styles.productContainer}>
-                            <h1>{elem.name}</h1>
-                            <h2>{elem.price}</h2>
+                            <abbr title={elem.name}>
+                                <h1>{elem.name}</h1>
+                            </abbr>
+                            <h2>${elem.price.toFixed(2)}</h2>
                             <button>Add ocurrences</button>
                         </div> 
                     ))
