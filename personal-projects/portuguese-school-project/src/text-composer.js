1de6981b7e8bebdbd00d9a848af6c01fac5e74df
Joao <dev.joaogustavo.dev@gmail.com>
2024-02-15 20:16:10 -0300

    school project | stored texts for being used in the application

--- personal-projects/portuguese-school-project/src/text-composer.js
+++ personal-projects/portuguese-school-project/src/text-composer.js
@@ -1,3 +1,12 @@
+var CONTAINER_FOR_TARGET_TEXT = document.querySelector(".game-target-text-container__target-text");
+function render(mode, target, innerValue) {
+    if (mode === "modify") {
+        target.innerHTML = innerValue.toString();
+    }
+    else {
+        target.innerHTML += innerValue.toString();
+    }
+}
 var TargetText = /** @class */ (function () {
     function TargetText() {
         this.content = this.getText();
@@ -10,25 +19,55 @@ var TargetText = /** @class */ (function () {
                 element.alreadyUsed = true;
             }
         });
+        render("modify", CONTAINER_FOR_TARGET_TEXT, reachedText);
         return reachedText;
     };
     TargetText.TEXTS = [
         {
-            text: "1O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
+            text: "O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
             alreadyUsed: false,
         },
         {
-            text: "2O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
+            text: "Alicia tem um tênis caríssimo!",
             alreadyUsed: false,
         },
         {
-            text: "3O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
+            text: "Os Flintstones têm um carro de pedra.",
             alreadyUsed: false,
         },
         {
-            text: "4O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
+            text: "A mensagem vem de muito longe...",
+            alreadyUsed: false,
+        },
+        {
+            text: "Os tripulantes vêm de lugares distantes!",
+            alreadyUsed: false,
+        },
+        {
+            text: "Minha família me mantém até eu arranjar um emprego.",
+            alreadyUsed: false,
+        },
+        {
+            text: "Aquelas senhoras mantêm segredos abomináveis!",
+            alreadyUsed: false,
+        },
+        {
+            text: "Ele só colabora quando o convém.",
+            alreadyUsed: false,
+        },
+        {
+            text: "Quando lhe convêm, você se interessa por minhas histórias.",
+            alreadyUsed: false,
+        },
+        {
+            text: "Hoje, ele não nos detém, uma vez que, ontem, já não pôde.",
+            alreadyUsed: false,
+        },
+        {
+            text: "Nesse momento, os policiais não nos detêm em casa, pois não têm um mandado.",
             alreadyUsed: false,
         },
     ];
     return TargetText;
 }());
+new TargetText();
--- personal-projects/portuguese-school-project/src/text-composer.ts
+++ personal-projects/portuguese-school-project/src/text-composer.ts
@@ -1,19 +1,63 @@
+const CONTAINER_FOR_TARGET_TEXT = document.querySelector(
+  ".game-target-text-container__target-text"
+)!;
+
+function render(
+  mode: "modify" | "keep",
+  target: Element,
+  innerValue: string | number
+) {
+  if (mode === "modify") {
+    target.innerHTML = innerValue.toString();
+  } else {
+    target.innerHTML += innerValue.toString();
+  }
+}
+
 class TargetText {
   static TEXTS = [
     {
-      text: "1O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
+      text: "O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
       alreadyUsed: false,
     },
     {
-      text: "2O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
+      text: "Alicia tem um tênis caríssimo!",
       alreadyUsed: false,
     },
     {
-      text: "3O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
+      text: "Os Flintstones têm um carro de pedra.",
       alreadyUsed: false,
     },
     {
-      text: "4O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
+      text: "A mensagem vem de muito longe...",
+      alreadyUsed: false,
+    },
+    {
+      text: "Os tripulantes vêm de lugares distantes!",
+      alreadyUsed: false,
+    },
+    {
+      text: "Minha família me mantém até eu arranjar um emprego.",
+      alreadyUsed: false,
+    },
+    {
+      text: "Aquelas senhoras mantêm segredos abomináveis!",
+      alreadyUsed: false,
+    },
+    {
+      text: "Ele só colabora quando o convém.",
+      alreadyUsed: false,
+    },
+    {
+      text: "Quando lhe convêm, você se interessa por minhas histórias.",
+      alreadyUsed: false,
+    },
+    {
+      text: "Hoje, ele não nos detém, uma vez que, ontem, já não pôde.",
+      alreadyUsed: false,
+    },
+    {
+      text: "Nesse momento, os policiais não nos detêm em casa, pois não têm um mandado.",
       alreadyUsed: false,
     },
   ];
@@ -27,14 +71,18 @@ class TargetText {
   private getText() {
     let availableTexts = TargetText.TEXTS.filter((text) => !text.alreadyUsed);
 
-    let reachedText = availableTexts[Math.floor(Math.random() * availableTexts.length)].text;
+    let reachedText =
+      availableTexts[Math.floor(Math.random() * availableTexts.length)].text;
 
     TargetText.TEXTS.forEach((element) => {
       if (element.text === reachedText) {
         element.alreadyUsed = true;
       }
-    })
+    });
 
+    render("modify", CONTAINER_FOR_TARGET_TEXT, reachedText);
     return reachedText;
   }
 }
+
+new TargetText();
