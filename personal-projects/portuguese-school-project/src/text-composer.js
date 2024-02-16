00f736fc5aa408039113e5f9fd668b8b52d13c08
Joao <dev.joaogustavo.dev@gmail.com>
2024-02-16 16:57:16 -0300

    school project | created a method for removing text accentuation, store and display it

--- personal-projects/portuguese-school-project/src/text-composer.js
+++ personal-projects/portuguese-school-project/src/text-composer.js
@@ -9,62 +9,95 @@ function render(mode, target, innerValue) {
 }
 var TargetText = /** @class */ (function () {
     function TargetText() {
-        this.content = this.getText();
+        this.formatContent = "";
+        this.mutableWords = [];
+        this.getNewText();
+        this.unaccentuateText();
+        render("modify", CONTAINER_FOR_TARGET_TEXT, this.formatContent);
     }
-    TargetText.prototype.getText = function () {
+    TargetText.prototype.getNewText = function () {
         var availableTexts = TargetText.TEXTS.filter(function (text) { return !text.alreadyUsed; });
-        var reachedText = availableTexts[Math.floor(Math.random() * availableTexts.length)].text;
+        var reachedText = availableTexts[Math.floor(Math.random() * availableTexts.length)];
+        this.mutableWords = reachedText.multableKeywords;
         TargetText.TEXTS.forEach(function (element) {
-            if (element.text === reachedText) {
+            if (element.text === reachedText.text) {
                 element.alreadyUsed = true;
             }
         });
-        render("modify", CONTAINER_FOR_TARGET_TEXT, reachedText);
-        return reachedText;
+        this.content = reachedText.text;
+    };
+    TargetText.prototype.unaccentuateText = function () {
+        var _this = this;
+        var TEXT_WORDS = this.content.split(" ");
+        TEXT_WORDS.forEach(function (word) {
+            var _a;
+            if ((_a = _this.mutableWords) === null || _a === void 0 ? void 0 : _a.includes(word)) {
+                word = _this.unaccentuateWord(word);
+            }
+            _this.formatContent += word + " ";
+        });
+    };
+    TargetText.prototype.unaccentuateWord = function (word) {
+        var POSSIBLE_ACCENTUATIONS = "áâéêíóôúû";
+        var UNACCENTUATIONS = "aaeeioouu";
+        var WORD_CHARS = Array.from(word);
+        word = "";
+        WORD_CHARS.forEach(function (char) {
+            if (POSSIBLE_ACCENTUATIONS.indexOf(char) > -1) {
+                char = UNACCENTUATIONS[POSSIBLE_ACCENTUATIONS.indexOf(char)];
+            }
+            word += char;
+        });
+        return word;
     };
     TargetText.TEXTS = [
-        {
-            text: "O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
-            alreadyUsed: false,
-        },
         {
             text: "Alicia tem um tênis caríssimo!",
             alreadyUsed: false,
         },
         {
             text: "Os Flintstones têm um carro de pedra.",
+            multableKeywords: ["têm"],
             alreadyUsed: false,
         },
         {
             text: "A mensagem vem de muito longe...",
+            multableKeywords: [],
             alreadyUsed: false,
         },
         {
             text: "Os tripulantes vêm de lugares distantes!",
+            multableKeywords: ["vêm"],
             alreadyUsed: false,
         },
         {
             text: "Minha família me mantém até eu arranjar um emprego.",
+            multableKeywords: ["mantém"],
             alreadyUsed: false,
         },
         {
             text: "Aquelas senhoras mantêm segredos abomináveis!",
+            multableKeywords: ["mantêm"],
             alreadyUsed: false,
         },
         {
             text: "Ele só colabora quando o convém.",
+            multableKeywords: ["convém"],
             alreadyUsed: false,
         },
         {
             text: "Quando lhe convêm, você se interessa por minhas histórias.",
+            multableKeywords: ["convêm,"],
             alreadyUsed: false,
         },
         {
             text: "Hoje, ele não nos detém, uma vez que, ontem, já não pôde.",
+            multableKeywords: ["detém,", "pôde."],
             alreadyUsed: false,
         },
         {
             text: "Nesse momento, os policiais não nos detêm em casa, pois não têm um mandado.",
+            multableKeywords: ["detêm", "têm"],
             alreadyUsed: false,
         },
     ];
--- personal-projects/portuguese-school-project/src/text-composer.ts
+++ personal-projects/portuguese-school-project/src/text-composer.ts
@@ -17,71 +17,109 @@ function render(
 class TargetText {
   static TEXTS = [
     {
-      text: "O sabiá disse que sabia assoviar, mas quando o sabiá viu o sábio, o sábio disse que o sabiá não sabia assoviar.",
-      alreadyUsed: false,
-    },
-    {
       text: "Alicia tem um tênis caríssimo!",
       alreadyUsed: false,
     },
     {
       text: "Os Flintstones têm um carro de pedra.",
+      multableKeywords: ["têm"],
       alreadyUsed: false,
     },
     {
       text: "A mensagem vem de muito longe...",
+      multableKeywords: [],
       alreadyUsed: false,
     },
     {
       text: "Os tripulantes vêm de lugares distantes!",
+      multableKeywords: ["vêm"],
       alreadyUsed: false,
     },
     {
       text: "Minha família me mantém até eu arranjar um emprego.",
+      multableKeywords: ["mantém"],
       alreadyUsed: false,
     },
     {
       text: "Aquelas senhoras mantêm segredos abomináveis!",
+      multableKeywords: ["mantêm"],
       alreadyUsed: false,
     },
     {
       text: "Ele só colabora quando o convém.",
+      multableKeywords: ["convém"],
       alreadyUsed: false,
     },
     {
       text: "Quando lhe convêm, você se interessa por minhas histórias.",
+      multableKeywords: ["convêm,"],
       alreadyUsed: false,
     },
     {
       text: "Hoje, ele não nos detém, uma vez que, ontem, já não pôde.",
+      multableKeywords: ["detém,", "pôde."],
       alreadyUsed: false,
     },
     {
       text: "Nesse momento, os policiais não nos detêm em casa, pois não têm um mandado.",
+      multableKeywords: ["detêm", "têm"],
       alreadyUsed: false,
     },
   ];
 
-  public content;
+  public content: string;
+  public formatContent: string = "";
+  private mutableWords: Array<string> | undefined = [];
 
   constructor() {
-    this.content = this.getText();
+    this.getNewText();
+    this.unaccentuateText();
+    render("modify", CONTAINER_FOR_TARGET_TEXT, this.formatContent);
   }
 
-  private getText() {
+  private getNewText() {
     let availableTexts = TargetText.TEXTS.filter((text) => !text.alreadyUsed);
 
     let reachedText =
-      availableTexts[Math.floor(Math.random() * availableTexts.length)].text;
+      availableTexts[Math.floor(Math.random() * availableTexts.length)];
+    this.mutableWords = reachedText.multableKeywords;
 
     TargetText.TEXTS.forEach((element) => {
-      if (element.text === reachedText) {
+      if (element.text === reachedText.text) {
         element.alreadyUsed = true;
       }
     });
 
-    render("modify", CONTAINER_FOR_TARGET_TEXT, reachedText);
-    return reachedText;
+    this.content = reachedText.text;
+  }
+
+  
+  private unaccentuateText() {
+    const TEXT_WORDS: string[] = this.content.split(" ");
+    
+    TEXT_WORDS.forEach((word) => {
+      if (this.mutableWords?.includes(word)) {
+        word = this.unaccentuateWord(word);
+      }
+      this.formatContent += word + " ";
+    });
+  }
+  
+  private unaccentuateWord(word: string) {
+    const POSSIBLE_ACCENTUATIONS = "áâéêíóôúû";
+    const UNACCENTUATIONS = "aaeeioouu";
+  
+    const WORD_CHARS = Array.from(word);
+  
+    word = "";
+    WORD_CHARS.forEach((char) => {
+      if (POSSIBLE_ACCENTUATIONS.indexOf(char) > -1) {
+        char = UNACCENTUATIONS[POSSIBLE_ACCENTUATIONS.indexOf(char)];
+      }
+      word += char;
+    });
+  
+    return word;
   }
 }
 
