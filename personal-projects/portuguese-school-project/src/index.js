bab5b9773aa1144ed30ac2c19ee00f84816435ff
Joao <dev.joaogustavo.dev@gmail.com>
2024-02-17 10:50:07 -0300

    school project | united the whole application code in a single file

--- personal-projects/portuguese-school-project/src/index.js
+++ personal-projects/portuguese-school-project/src/index.js
@@ -1,5 +1,3 @@
-var PLAYERS_SCORES_TXTS = document.getElementsByClassName("pontuations-container__player-pontuation__text");
-var CONTAINER_FOR_ALL_PLAYERS_SCORES = document.querySelector(".pontuations-container");
 function render(mode, target, innerValue) {
     if (mode === "modify") {
         target.innerHTML = innerValue.toString();
@@ -8,6 +6,8 @@ function render(mode, target, innerValue) {
         target.innerHTML += innerValue.toString();
     }
 }
+var PLAYERS_SCORES_TXTS = document.getElementsByClassName("pontuations-container__player-pontuation__text");
+var CONTAINER_FOR_ALL_PLAYERS_SCORES = document.querySelector(".pontuations-container");
 var PlayersScore = /** @class */ (function () {
     function PlayersScore(emoji, score, index) {
         if (score === void 0) { score = 0; }
@@ -68,7 +68,225 @@ var PlayersScore = /** @class */ (function () {
     PlayersScore.players = [];
     return PlayersScore;
 }());
-new PlayersScore("🤩");
-new PlayersScore("😀", 10);
-PlayersScore.updateScore("increase", "😀", 2);
-PlayersScore.setMajorScorePlayerCrown();
+var CONTAINER_FOR_TARGET_TEXT = document.querySelector(".game-target-text-container__target-text");
+var EDITABLE_WORDS_ELEMENTS = document.getElementsByClassName("editable-char");
+var TargetText = /** @class */ (function () {
+    function TargetText() {
+        this.mutableWords = [];
+        this.getNewText();
+        this.unaccentuateText();
+        this.displayText();
+    }
+    TargetText.prototype.getNewText = function () {
+        var availableTexts = TargetText.TEXTS.filter(function (text) { return !text.alreadyUsed; });
+        var reachedText = availableTexts[Math.floor(Math.random() * availableTexts.length)];
+        this.mutableWords = reachedText.multableKeywords;
+        TargetText.TEXTS.forEach(function (element) {
+            if (element.text === reachedText.text) {
+                element.alreadyUsed = true;
+            }
+        });
+        TargetText.content = reachedText.text;
+    };
+    TargetText.prototype.unaccentuateText = function () {
+        var _this = this;
+        var TEXT_WORDS = TargetText.content.split(" ");
+        TEXT_WORDS.forEach(function (word) {
+            var _a;
+            if ((_a = _this.mutableWords) === null || _a === void 0 ? void 0 : _a.includes(word)) {
+                word = _this.unaccentuateWord(word);
+            }
+            TargetText.formatedContent += word + " ";
+        });
+    };
+    TargetText.prototype.unaccentuateWord = function (word) {
+        var POSSIBLE_ACCENTUATIONS = "áâéêíóôúû";
+        var UNACCENTUATIONS = "aaeeioouu";
+        var WORD_CHARS = Array.from(word);
+        word = "";
+        WORD_CHARS.forEach(function (char) {
+            if (POSSIBLE_ACCENTUATIONS.indexOf(char) > -1) {
+                char = UNACCENTUATIONS[POSSIBLE_ACCENTUATIONS.indexOf(char)];
+            }
+            word += char;
+        });
+        return word;
+    };
+    TargetText.prototype.displayText = function () {
+        var _this = this;
+        render("modify", CONTAINER_FOR_TARGET_TEXT, "");
+        var TEXT_WORDS = TargetText.formatedContent.split(" ");
+        TEXT_WORDS.forEach(function (word) {
+            var newWordElement = "<div class=\"full-word\">";
+            var WORD_CHARS = Array.from(word);
+            WORD_CHARS.forEach(function (char, index) {
+                newWordElement += _this.createEditableCharElement(char);
+            });
+            newWordElement += "</div>";
+            render("keep", CONTAINER_FOR_TARGET_TEXT, newWordElement);
+        });
+        this.setEditableElementsEventListeners();
+    };
+    TargetText.prototype.getCharPossibleAccentuations = function (entryChar) {
+        var POSSIBLE_ACCENTUATIONS = "áâéêíîóôúû";
+        var UNACCENTUATIONS = "aaeeiioouu";
+        var CHAR_INDEX = POSSIBLE_ACCENTUATIONS.includes(entryChar.toLowerCase())
+            ? POSSIBLE_ACCENTUATIONS.indexOf(entryChar.toLowerCase())
+            : UNACCENTUATIONS.indexOf(entryChar.toLowerCase());
+        var isCircumflex = CHAR_INDEX % 2 !== 0;
+        return [
+            UNACCENTUATIONS[CHAR_INDEX],
+            isCircumflex
+                ? POSSIBLE_ACCENTUATIONS[CHAR_INDEX - 1]
+                : POSSIBLE_ACCENTUATIONS[CHAR_INDEX],
+            isCircumflex
+                ? entryChar.toLowerCase()
+                : POSSIBLE_ACCENTUATIONS[CHAR_INDEX + 1],
+        ];
+    };
+    TargetText.prototype.createEditableCharElement = function (char) {
+        var VOWELS = "aeiouáâéêíóôúû";
+        var CONTAINER_SIGNS_BUTTONS = "";
+        this.getCharPossibleAccentuations(char).forEach(function (accentuatedChar) {
+            CONTAINER_SIGNS_BUTTONS += "<button class=\"editable-char__edit-container__buttons\" onClick=\"TargetText.editAccentuation(this)\">".concat(accentuatedChar, "</button>");
+        });
+        if (" .,!".includes(char)) {
+            return char;
+        }
+        return "<span class=\"editable-char\">".concat(char, "<span class=\"editable-char__edit-container\">\n      ").concat(VOWELS.includes(char.toLowerCase())
+            ? CONTAINER_SIGNS_BUTTONS
+            : "Acento aqui não! ❌", "<div class=\"editable-char__edit-container__index\"></div></span></span>");
+    };
+    TargetText.prototype.setEditableElementsEventListeners = function () {
+        if (EDITABLE_WORDS_ELEMENTS) {
+            Array.from(EDITABLE_WORDS_ELEMENTS).forEach(function (element) {
+                element.addEventListener("click", function (event) {
+                    TargetText.setWordEditableMode(event.target);
+                });
+            });
+        }
+    };
+    TargetText.editAccentuation = function (selectedAccentuation) {
+        var _a;
+        var currentCharElement = (_a = selectedAccentuation.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode;
+        currentCharElement.innerHTML = currentCharElement.innerHTML.replace(currentCharElement.innerHTML[0], selectedAccentuation.innerHTML);
+    };
+    TargetText.setWordEditableMode = function (targetWordElement) {
+        if (targetWordElement.classList.contains("editing-char")) {
+            targetWordElement.classList.remove("editing-char");
+            return;
+        }
+        Array.from(EDITABLE_WORDS_ELEMENTS).forEach(function (element) {
+            element.classList.remove("editing-char");
+        });
+        targetWordElement.classList.add("editing-char");
+    };
+    TargetText.TEXTS = [
+        {
+            text: "Alicia tem um tênis caríssimo!",
+            alreadyUsed: false,
+        },
+        {
+            text: "Os Flintstones têm um carro de pedra.",
+            multableKeywords: ["têm"],
+            alreadyUsed: false,
+        },
+        {
+            text: "A mensagem vem de muito longe...",
+            multableKeywords: [],
+            alreadyUsed: false,
+        },
+        {
+            text: "Os tripulantes vêm de lugares distantes!",
+            multableKeywords: ["vêm"],
+            alreadyUsed: false,
+        },
+        {
+            text: "Minha família me mantém até eu arranjar um emprego.",
+            multableKeywords: ["mantém"],
+            alreadyUsed: false,
+        },
+        {
+            text: "Aquelas senhoras mantêm segredos abomináveis!",
+            multableKeywords: ["mantêm"],
+            alreadyUsed: false,
+        },
+        {
+            text: "Ele só colabora quando o convém.",
+            multableKeywords: ["convém."],
+            alreadyUsed: false,
+        },
+        {
+            text: "Quando lhe convêm, você se interessa por minhas histórias.",
+            multableKeywords: ["convêm,"],
+            alreadyUsed: false,
+        },
+        {
+            text: "Hoje, ele não nos detém, uma vez que, ontem, já não pôde.",
+            multableKeywords: ["detém,", "pôde."],
+            alreadyUsed: false,
+        },
+        {
+            text: "Nesse momento, os policiais não nos detêm em casa, pois não têm um mandado.",
+            multableKeywords: ["detêm", "têm"],
+            alreadyUsed: false,
+        },
+    ];
+    TargetText.formatedContent = "";
+    return TargetText;
+}());
+new TargetText();
+var MainButton = /** @class */ (function () {
+    function MainButton() {
+        MainButton.setButtonListener();
+    }
+    MainButton.setButtonListener = function () {
+        this.BUTTON_ELEMENT.addEventListener("click", function () {
+            MainButton.disableInteractableCharElements();
+            MainButton.compareTexts(MainButton.getCorrectTextChars(), MainButton.getEditedTextChars());
+        });
+    };
+    MainButton.disableInteractableCharElements = function () {
+        Array.from(document.getElementsByClassName("editable-char")).map(function (element) {
+            return element.classList.add("off");
+        });
+    };
+    MainButton.getCorrectTextChars = function () {
+        return Array.from(TargetText.content.toLowerCase()).filter(function (char) { return !" .,!".includes(char); });
+    };
+    MainButton.getEditedTextChars = function () {
+        return Array.from(document.getElementsByClassName("editable-char")).map(function (element) { return element.innerHTML[0].toLowerCase(); });
+    };
+    MainButton.compareTexts = function (modelText, analisedText) {
+        modelText.forEach(function (char, index) {
+            if (char !== analisedText[index]) {
+                MainButton.setWordStatus(index, "error");
+            }
+            else {
+                MainButton.setWordStatus(index, "correct");
+            }
+        });
+    };
+    MainButton.setWordStatus = function (index, status) {
+        var _a, _b;
+        var allChars = document.getElementsByClassName("editable-char");
+        var targetChar = allChars[index];
+        var targetCharEditContainer = targetChar.childNodes[1];
+        if (status === "correct") {
+            (_a = targetChar === null || targetChar === void 0 ? void 0 : targetChar.parentElement) === null || _a === void 0 ? void 0 : _a.classList.add("correct-word");
+        }
+        else {
+            targetChar.classList.add("error-char");
+            MainButton.setEditContainerCorrection(targetCharEditContainer, index);
+            (_b = targetChar === null || targetChar === void 0 ? void 0 : targetChar.parentElement) === null || _b === void 0 ? void 0 : _b.classList.add("error-word");
+        }
+    };
+    MainButton.setEditContainerCorrection = function (charEditContainer, index) {
+        var correctChar = MainButton.getCorrectTextChars()[index].toUpperCase();
+        charEditContainer.innerHTML =
+            "<p class=\"editable-char__edit-container__paragraph\">".concat(correctChar, " \uD83D\uDC48</p><div class=\"editable-char__edit-container__index\"></div>");
+    };
+    MainButton.BUTTON_ELEMENT = document.querySelector(".check-result-button");
+    return MainButton;
+}());
+new MainButton();
--- personal-projects/portuguese-school-project/src/index.ts
+++ personal-projects/portuguese-school-project/src/index.ts
@@ -1,9 +1,3 @@
-const PLAYERS_SCORES_TXTS = document.getElementsByClassName(
-  "pontuations-container__player-pontuation__text"
-);
-
-const CONTAINER_FOR_ALL_PLAYERS_SCORES = document.querySelector(".pontuations-container")!;
-
 function render(
   mode: "modify" | "keep",
   target: Element,
@@ -15,6 +9,12 @@ function render(
     target.innerHTML += innerValue.toString();
   }
 }
+
+const PLAYERS_SCORES_TXTS = document.getElementsByClassName(
+  "pontuations-container__player-pontuation__text"
+);
+
+const CONTAINER_FOR_ALL_PLAYERS_SCORES = document.querySelector(".pontuations-container")!;
 
 class PlayersScore {
   static quantityOfPlayersInGame = 0;
@@ -104,7 +104,287 @@ class PlayersScore {
   }
 }
 
-new PlayersScore("🤩");
-new PlayersScore("😀", 10);
-PlayersScore.updateScore("increase", "😀", 2);
-PlayersScore.setMajorScorePlayerCrown();
+
+const CONTAINER_FOR_TARGET_TEXT = document.querySelector(
+  ".game-target-text-container__target-text"
+)!;
+const EDITABLE_WORDS_ELEMENTS =
+  document.getElementsByClassName("editable-char");
+
+class TargetText {
+  static TEXTS = [
+    {
+      text: "Alicia tem um tênis caríssimo!",
+      alreadyUsed: false,
+    },
+    {
+      text: "Os Flintstones têm um carro de pedra.",
+      multableKeywords: ["têm"],
+      alreadyUsed: false,
+    },
+    {
+      text: "A mensagem vem de muito longe...",
+      multableKeywords: [],
+      alreadyUsed: false,
+    },
+    {
+      text: "Os tripulantes vêm de lugares distantes!",
+      multableKeywords: ["vêm"],
+      alreadyUsed: false,
+    },
+    {
+      text: "Minha família me mantém até eu arranjar um emprego.",
+      multableKeywords: ["mantém"],
+      alreadyUsed: false,
+    },
+    {
+      text: "Aquelas senhoras mantêm segredos abomináveis!",
+      multableKeywords: ["mantêm"],
+      alreadyUsed: false,
+    },
+    {
+      text: "Ele só colabora quando o convém.",
+      multableKeywords: ["convém."],
+      alreadyUsed: false,
+    },
+    {
+      text: "Quando lhe convêm, você se interessa por minhas histórias.",
+      multableKeywords: ["convêm,"],
+      alreadyUsed: false,
+    },
+    {
+      text: "Hoje, ele não nos detém, uma vez que, ontem, já não pôde.",
+      multableKeywords: ["detém,", "pôde."],
+      alreadyUsed: false,
+    },
+    {
+      text: "Nesse momento, os policiais não nos detêm em casa, pois não têm um mandado.",
+      multableKeywords: ["detêm", "têm"],
+      alreadyUsed: false,
+    },
+  ];
+
+  static content: string;
+  static formatedContent: string = "";
+  private mutableWords: Array<string> | undefined = [];
+
+  constructor() {
+    this.getNewText();
+    this.unaccentuateText();
+    this.displayText();
+  }
+
+  private getNewText() {
+    let availableTexts = TargetText.TEXTS.filter((text) => !text.alreadyUsed);
+
+    let reachedText =
+      availableTexts[Math.floor(Math.random() * availableTexts.length)];
+    this.mutableWords = reachedText.multableKeywords;
+
+    TargetText.TEXTS.forEach((element) => {
+      if (element.text === reachedText.text) {
+        element.alreadyUsed = true;
+      }
+    });
+
+    TargetText.content = reachedText.text;
+  }
+
+  private unaccentuateText() {
+    const TEXT_WORDS: string[] = TargetText.content.split(" ");
+
+    TEXT_WORDS.forEach((word) => {
+      if (this.mutableWords?.includes(word)) {
+        word = this.unaccentuateWord(word);
+      }
+      TargetText.formatedContent += word + " ";
+    });
+  }
+
+  private unaccentuateWord(word: string) {
+    const POSSIBLE_ACCENTUATIONS = "áâéêíóôúû";
+    const UNACCENTUATIONS = "aaeeioouu";
+
+    const WORD_CHARS = Array.from(word);
+
+    word = "";
+    WORD_CHARS.forEach((char) => {
+      if (POSSIBLE_ACCENTUATIONS.indexOf(char) > -1) {
+        char = UNACCENTUATIONS[POSSIBLE_ACCENTUATIONS.indexOf(char)];
+      }
+      word += char;
+    });
+
+    return word;
+  }
+
+  private displayText() {
+    render("modify", CONTAINER_FOR_TARGET_TEXT, "");
+    const TEXT_WORDS = TargetText.formatedContent.split(" ");
+
+    TEXT_WORDS.forEach((word) => {
+      let newWordElement = `<div class="full-word">`;
+
+      let WORD_CHARS = Array.from(word);
+      WORD_CHARS.forEach((char, index) => {
+        newWordElement += this.createEditableCharElement(char);
+      });
+
+      newWordElement += `</div>`;
+
+      render("keep", CONTAINER_FOR_TARGET_TEXT, newWordElement);
+    });
+
+    this.setEditableElementsEventListeners();
+  }
+
+  private getCharPossibleAccentuations(entryChar: string) {
+    const POSSIBLE_ACCENTUATIONS = "áâéêíîóôúû";
+    const UNACCENTUATIONS = "aaeeiioouu";
+
+    const CHAR_INDEX = POSSIBLE_ACCENTUATIONS.includes(entryChar.toLowerCase())
+      ? POSSIBLE_ACCENTUATIONS.indexOf(entryChar.toLowerCase())
+      : UNACCENTUATIONS.indexOf(entryChar.toLowerCase());
+
+    const isCircumflex = CHAR_INDEX % 2 !== 0;
+
+    return [
+      UNACCENTUATIONS[CHAR_INDEX],
+      isCircumflex
+        ? POSSIBLE_ACCENTUATIONS[CHAR_INDEX - 1]
+        : POSSIBLE_ACCENTUATIONS[CHAR_INDEX],
+      isCircumflex
+        ? entryChar.toLowerCase()
+        : POSSIBLE_ACCENTUATIONS[CHAR_INDEX + 1],
+    ];
+  }
+
+  private createEditableCharElement(char: string) {
+    const VOWELS = "aeiouáâéêíóôúû";
+    let CONTAINER_SIGNS_BUTTONS = "";
+
+    this.getCharPossibleAccentuations(char).forEach((accentuatedChar) => {
+      CONTAINER_SIGNS_BUTTONS += `<button class="editable-char__edit-container__buttons" onClick="TargetText.editAccentuation(this)">${accentuatedChar}</button>`;
+    });
+
+    if (" .,!".includes(char)) {
+      return char;
+    }
+
+    return `<span class="editable-char">${char}<span class="editable-char__edit-container">
+      ${
+        VOWELS.includes(char.toLowerCase())
+          ? CONTAINER_SIGNS_BUTTONS
+          : "Acento aqui não! ❌"
+      }<div class="editable-char__edit-container__index"></div></span></span>`;
+  }
+
+  private setEditableElementsEventListeners() {
+    if (EDITABLE_WORDS_ELEMENTS) {
+      Array.from(EDITABLE_WORDS_ELEMENTS).forEach((element) => {
+        (element as HTMLSpanElement).addEventListener(
+          "click",
+          function (event) {
+            TargetText.setWordEditableMode(event.target as HTMLSpanElement);
+          }
+        );
+      });
+    }
+  }
+
+  static editAccentuation(selectedAccentuation: HTMLElement) {
+    let currentCharElement = selectedAccentuation.parentNode?.parentNode;
+    (currentCharElement as HTMLElement).innerHTML = (
+      currentCharElement as HTMLElement
+    ).innerHTML.replace(
+      (currentCharElement as HTMLElement).innerHTML[0],
+      selectedAccentuation.innerHTML
+    );
+  }
+
+  static setWordEditableMode(targetWordElement: HTMLSpanElement) {
+    if (targetWordElement.classList.contains("editing-char")) {
+      targetWordElement.classList.remove("editing-char");
+      return;
+    }
+  
+    Array.from(EDITABLE_WORDS_ELEMENTS).forEach((element) => {
+      (element as HTMLSpanElement).classList.remove("editing-char");
+    });
+  
+    targetWordElement.classList.add("editing-char");
+  }
+}
+
+new TargetText();
+
+class MainButton {
+  static readonly BUTTON_ELEMENT: HTMLElement = document.querySelector(
+    ".check-result-button"
+  )!
+
+  constructor() {
+    MainButton.setButtonListener()
+  }
+
+  static setButtonListener() {
+    this.BUTTON_ELEMENT.addEventListener("click", function () {
+      MainButton.disableInteractableCharElements();
+    
+      MainButton.compareTexts(MainButton.getCorrectTextChars(), MainButton.getEditedTextChars());
+    });
+  }
+
+  static disableInteractableCharElements() {
+    Array.from(document.getElementsByClassName("editable-char")).map((element) =>
+      element.classList.add("off")
+    );
+  }
+  
+  static getCorrectTextChars() {
+    return Array.from(TargetText.content.toLowerCase()).filter(
+      (char) => !" .,!".includes(char)
+    );
+  }
+  static getEditedTextChars() {
+    return Array.from(document.getElementsByClassName("editable-char")).map(
+      (element) => element.innerHTML[0].toLowerCase()
+    );
+  }
+  
+  static compareTexts(modelText: string[], analisedText: string[]) {
+    modelText.forEach((char: string, index: number) => {
+      if (char !== analisedText[index]) {
+        MainButton.setWordStatus(index, "error");
+      } else {
+        MainButton.setWordStatus(index, "correct");
+      }
+    });
+  }
+  static setWordStatus(index: number, status: "error" | "correct") {
+    let allChars = document.getElementsByClassName("editable-char");
+  
+    let targetChar = allChars[index]
+    let targetCharEditContainer = (targetChar.childNodes[1] as HTMLElement);
+  
+    if (status === "correct") {
+      targetChar?.parentElement?.classList.add("correct-word");
+    } else {
+      targetChar.classList.add("error-char");
+      MainButton.setEditContainerCorrection(targetCharEditContainer, index);
+    
+      targetChar?.parentElement?.classList.add("error-word");
+    }
+  } 
+  
+  static setEditContainerCorrection(charEditContainer: HTMLElement, index: number) {
+    let correctChar = MainButton.getCorrectTextChars()[index].toUpperCase();
+  
+    charEditContainer.innerHTML = 
+    `<p class="editable-char__edit-container__paragraph">${correctChar} 👈</p><div class="editable-char__edit-container__index"></div>`
+  }
+}
+new MainButton();
+
+
+
